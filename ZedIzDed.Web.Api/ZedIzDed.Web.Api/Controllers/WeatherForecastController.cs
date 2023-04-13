d313c0373fbbbe5363fcf602073db988f3d6eb8b
DESKTOP-KJD3JEG\gerem <geremy@ergonomicware.com>
2023-04-13 13:02:56 -0700

    First dev commit

--- ZedIzDed.Web.Api/ZedIzDed.Web.Api/Controllers/WeatherForecastController.cs
+++ ZedIzDed.Web.Api/ZedIzDed.Web.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,17 @@ namespace ZedIzDed.Web.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Absolute Zero", 
+            "Freezing", 
+            "Bracing", 
+            "Chilly", 
+            "Cool", 
+            "Mild", 
+            "Warm", 
+            "Balmy", 
+            "Hot", 
+            "Sweltering", 
+            "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
